6f3336c88e6c530081cab849731b118daa7d1bb3
Martin Roy <mroy@internap.com>
2017-12-13 10:13:38 -0500

    2017

--- 2017/day11.py
+++ 2017/day11.py
@@ -85,3 +85,11 @@ if __name__ == '__main__':
         result = compute(puzzle_input)
 
     print("Result is {}".format(result))
+
+
+# -1, 2     1, 1      3,0
+#      0,1       2,0
+# -1,1      1,0       3,-1
+#      0,0       2,-1
+# -1,0      1,-1      3,-2
+#      0,-1      2,-2
