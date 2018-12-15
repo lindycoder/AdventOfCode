96454aa4ccd38d2ceb4d4f8e8067ff56d1c6db7f
Martin Roy <mroy@internap.com>
2018-12-14 22:32:36 -0500

    Day 13 2018

--- y2018/__init__.py
+++ y2018/__init__.py
@@ -8,3 +8,12 @@ class Point:
 
     def manhattan_dist(self, other):
         return abs(self.x - other.x) + abs(self.y - other.y)
+
+    def __add__(self, other):
+        return Point(self.x + other.x, self.y + other.y)
+
+    def __lt__(self, other):
+        return self.tuple() < other.tuple()
+
+    def tuple(self):
+        return self.x, self.y
